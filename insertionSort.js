7f2be8a47765db476e7a28f6001db957f5e4e6e2
Adam <acdoyle630@gmail.com>
2017-04-06 11:58:01 -1000

    cleaned debugger and CL

--- insertionSort.js
+++ insertionSort.js
@@ -1,25 +1,32 @@
 /*jshint esversion: 6*/
-const testing = () => {
-  var current = 1;
-  function insertionSort( arr ){
-    if(current === arr.length){
-      return;
-    }
-    for(var i = current; i >=0; i-- ){
-      if (arr[current] > arr[ i - 1] || i - 1 === 0){
-        arr.splice(i-1, 0, arr[current]);
-        arr.splice(current + 1, 1);
-        }
+module.exports = () => {
+    var current = 1;
+    function insertionSort( arr ){
+      if(Array.isArray(arr) !== true ){
+        return false;
       }
-      current ++;
-      insertionSort(arr);
-      return arr;
-    }
-  return {
-    insertionSort,
-  };
+      if(current === arr.length){
+        return;
+      }
+      for(var i = current; i >=0; i-- ){
+      if(typeof arr[i] !== 'number' ){
+        return false;
+      }
+      if(arr[current] < arr[0]){
+        arr.splice(0, 0, arr[current]);
+        arr.splice(current +1, 1);
+      }
+        if (arr[current] < arr[ i - 1]  && arr[current] > arr[i - 2]){
+          arr.splice(i-1, 0, arr[current]);
+          arr.splice(current + 1, 1);
+          }
+        }
+        current ++;
+        insertionSort(arr);
+        return arr;
+      }
+    return {
+      insertionSort,
+    };
 };
 
-
-var test = testing();
-console.log(test.insertionSort([9,6,4,3,2,1]));
